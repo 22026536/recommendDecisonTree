2520948ee53eaf72824f49fc85499bb0fb646ab4
22026536 <22026536>
2024-11-26 21:22:15 +0700

    quang

--- decisiontree.py
+++ decisiontree.py
@@ -66,5 +66,5 @@ import uvicorn
 import os
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Render sẽ cung cấp cổng trong biến PORT
-    uvicorn.run("deciontree:app", host="0.0.0.0", port=port)
+    uvicorn.run("decisiontree:app", host="0.0.0.0", port=port)
 
